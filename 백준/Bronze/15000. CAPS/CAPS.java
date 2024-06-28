e4ad68b22b43581797169b05813a1fce22d815b4
GyeongMin1022 <scsoym33@gmail.com>
2024-06-28 15:50:02 +0900

    [Bronze V] Title: CAPS, Time: 452 ms, Memory: 27504 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String str = scan.next();
+
+		str = str.toUpperCase();
+
+		System.out.println(str);
+
+	}
+}
