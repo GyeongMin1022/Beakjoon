290a10652ec93503ef33f6a9458c2e99ced0fb09
GyeongMin1022 <scsoym33@gmail.com>
2024-07-03 09:37:01 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 172 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        long calc = 1;
+        for (int i = n; i > 0; i--) {
+            calc *= i;
+        }
+        System.out.println(calc);
+    }
+}
